c8f58449a951719dfab2d4625982baac362a0cab
youcef <youcef.93.g@gmail.com>
2020-07-01 21:53:42 +0100

    Fixed npm dependecies

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+const PORT = process.env.PORT;
 
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: process.env.PORT | 8080 });
+const wss = new WebSocket.Server({ port: PORT | 8080 });
 
 const Connection = require("./Connection.js");
 
@@ -13,14 +14,8 @@ wss.on("listening", ()=>{
 });
 
 wss.on('connection', function connection(ws, request) {
-  
   console.log("New Connection")
-
   const newConnection = new Connection(ws);
-
-  
 });
-
-
 
 
