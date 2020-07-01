dbe57fc365022429d280ab1c1ea830ca183e9a6a
youcef <youcef.93.g@gmail.com>
2020-07-01 22:47:30 +0100

    Change PORT to PRT in env

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PRT;
 
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: PORT || 3000 });
