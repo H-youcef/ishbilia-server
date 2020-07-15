315565a0a489e5c9cebc54a8388acf4206a66f3a
youcef <youcef.93.g@gmail.com>
2020-07-15 21:04:13 +0100

    Included the DbManager in index.js, now the websocket server won't listening untill the Db connection is established.

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+const DbManager = require('./DbManager');
+const dbManager = new DbManager();
+
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: process.env.PORT || 3000 , host: "0.0.0.0"});
-
 const Connection = require("./Connection.js");
 
+dbManager.connect(function(error){
+  if(error){
+    console.log("Failed to connect to Database server: ", error.message);
+    throw error;
+  }
+  console.log("Staring WebSocket Server...");
 
-wss.on("listening", ()=>{
-  console.log("The server is running.");
+  wss.on("listening", ()=>{
+    console.log("The server is running.");
+  });
+
+  wss.on('connection', function connection(ws, request) {
+    console.log("New Connection")
+    const newConnection = new Connection(ws);
+  });
+
 });
-
-wss.on('connection', function connection(ws, request) {
-  console.log("New Connection")
-  const newConnection = new Connection(ws);
-});
-
-
